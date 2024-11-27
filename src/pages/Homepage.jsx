5a717bae217337b0374b7fdeccd7172baeebdc96
khanhnvn <khanhnvn1205@gmail.com>
2024-11-27 13:20:25 +0700

    fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,7 @@ import {
 import { Navigate } from 'react-router-dom';
 
 const HomePage = ({ isLoggedIn }) => {
-    console.log("role id:", roleId);
+    console.log("isLoggedIn:", isLoggedIn);
     if (!isLoggedIn) {
         return <Navigate to="/login" />; 
     }
