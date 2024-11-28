f2cfd6cf1047132f9d0093e4e4331222f9ab3193
khanhnvn <khanhnvn1205@gmail.com>
2024-11-28 23:06:18 +0700

    fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -16,15 +16,15 @@ const HomePage = ({ }) => {
             <Navbar />
             <Box flex={1} display="flex" flexDirection="column" alignItems="center"> {/* Center content vertically */}
                 <Container maxW="container.lg" p={4} textAlign="center"> {/* Center text */}
-                    <VStack spacing={4} alignItems="center" mt={10}> {/* Add margin top */}
+                    <VStack spacing={2} alignItems="center" mt={4}> {/* Add margin top */}
                         <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Too busy?</Text>
                         <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Find a home?</Text>
                         <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Let us help</Text>
                     </VStack>
 
-                    <HStack spacing={4} mt={10}> {/* Add margin top */}
-                        <Image src="/image1.png" alt="Mèo" boxSize="200px" objectFit="cover" />
-                        <Image src="/image.png" alt="Chó" boxSize="200px" objectFit="cover" />
+                    <HStack spacing={4} mt={10} justifyContent="center"> {/* Center the images */}
+                        <Image src="/image1.png" alt="Mèo" h="200px" objectFit="cover" borderRadius="30px" /> {/* Adjusted height and border radius */}
+                        <Image src="/image.png" alt="Chó" h="200px" objectFit="cover" borderRadius="30px" /> {/* Adjusted height and border radius */}
                     </HStack>
                 </Container>
             </Box>
