e7f9f7868a1c5f2ea0592a1b50d284b173a82a4d
khanhnvn <khanhnvn1205@gmail.com>
2024-11-28 22:59:03 +0700

    fix

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,34 @@
-//Homepage.jsx
 import {
     Box,
     Container,
     Heading,
     Text,
-    VStack, 
+    VStack,
+    Image,
+    HStack,
 } from '@chakra-ui/react';
-import Navbar from './components/NavBar.jsx'; // Import Navbar component
+import Navbar from './components/NavBar.jsx';
 import Footer from "./components/Footer.jsx";
 
-const HomePage = ({}) => {
-    console.log("checkconsolelog");
+const HomePage = ({ }) => {
     return (
-        <Box bg="#FFFCF8" minHeight="100vh"
-             display="flex" flexDirection="column"
-        > {/* Đặt màu nền cho trang */}
+        <Box bg="#FFFCF8" minHeight="100vh" display="flex" flexDirection="column">
             <Navbar />
-            {/* Body */}
-            <Box flex={1}>
-                <Container maxW="container.lg" p={4} flex={1}>
-                    <VStack spacing={4} alignItems="flex-start">
-                        <Heading as="h1" size="xl">
-                            Welcome to Pet Guardian!
-                        </Heading>
-                        {/* Thêm nội dung cho body ở đây */}
-                        <Text>Đây là trang chủ của ứng dụng.</Text>
+            <Box flex={1} display="flex" flexDirection="column" alignItems="center"> {/* Center content vertically */}
+                <Container maxW="container.lg" p={4} textAlign="center"> {/* Center text */}
+                    <VStack spacing={4} alignItems="center" mt={10}> {/* Add margin top */}
+                        <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Too busy?</Text>
+                        <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Find a home?</Text>
+                        <Text fontSize="36px" fontWeight="semibold" color="#1A4742" fontFamily="Poppins">Let us help</Text>
                     </VStack>
+
+                    <HStack spacing={4} mt={10}> {/* Add margin top */}
+                        <Image src="/image1.png" alt="Mèo" boxSize="200px" objectFit="cover" />
+                        <Image src="/image.png" alt="Chó" boxSize="200px" objectFit="cover" />
+                    </HStack>
                 </Container>
             </Box>
             <Footer />
-
         </Box>
     );
 };
